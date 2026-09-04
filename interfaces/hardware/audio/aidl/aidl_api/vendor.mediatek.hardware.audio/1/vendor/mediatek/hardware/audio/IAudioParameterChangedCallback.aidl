mt6897-devs/hardware_mediatek

interfaces: Initial reverse engineer of vendor.mediatek.hardware.audio
Change-Id: I3840e7b19252332626f06bfe85c2e9dd7c2a649b
Signed-off-by: rio004 <riyon336@gmail.com>

interfaces/hardware/audio/aidl/aidl_api/vendor.mediatek.hardware.audio/1/vendor/mediatek/hardware/audio/IAudioParameterChangedCallback.aidl
@@ -1,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+///////////////////////////////////////////////////////////////////////////////
+// THIS FILE IS IMMUTABLE. DO NOT EDIT IN ANY CASE.                          //
+///////////////////////////////////////////////////////////////////////////////
+
+// This file is a snapshot of an AIDL file. Do not edit it manually. There are
+// two cases:
+// 1). this is a frozen version file - do not edit this in any case.
+// 2). this is a 'current' file. If you make a backwards compatible change to
+//     the interface (from the latest frozen version), the build system will
+//     prompt you to update this file with `m <name>-update-api`.
+//
+// You must not make a backward incompatible change to any AIDL file built
+// with the aidl_interface module type with versions property set. The module
+// type is used to build AIDL files in a way that they can be used across
+// independently updatable components of the system. If a device is shipped
+// with such a backward incompatible change, it has a high risk of breaking
+// later when a module using the interface is updated, e.g., Mainline modules.
+
+package vendor.mediatek.hardware.audio;
+@VintfStability
+interface IAudioParameterChangedCallback {
+  void onAudioParameterChanged(in String keyValuePairs);
+}

interfaces/hardware/audio/aidl/vendor/mediatek/hardware/audio/IAudioParameterChangedCallback.aidl
@@ -1,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package vendor.mediatek.hardware.audio;
+
+@VintfStability
+interface IAudioParameterChangedCallback {
+    void onAudioParameterChanged(in String keyValuePairs);
+}

Project-Hikari-MewType/MewType_hardware_mtk

Co-authored-by: Ben Grisdale <bengris32@protonmail.ch>

interfaces/hardware/audio/aidl/aidl_api/vendor.mediatek.hardware.audio/1/vendor/mediatek/hardware/audio/IAudioParameterChangedCallback.aidl
@@ -1,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+///////////////////////////////////////////////////////////////////////////////
+// THIS FILE IS IMMUTABLE. DO NOT EDIT IN ANY CASE.                          //
+///////////////////////////////////////////////////////////////////////////////
+
+// This file is a snapshot of an AIDL file. Do not edit it manually. There are
+// two cases:
+// 1). this is a frozen version file - do not edit this in any case.
+// 2). this is a 'current' file. If you make a backwards compatible change to
+//     the interface (from the latest frozen version), the build system will
+//     prompt you to update this file with `m <name>-update-api`.
+//
+// You must not make a backward incompatible change to any AIDL file built
+// with the aidl_interface module type with versions property set. The module
+// type is used to build AIDL files in a way that they can be used across
+// independently updatable components of the system. If a device is shipped
+// with such a backward incompatible change, it has a high risk of breaking
+// later when a module using the interface is updated, e.g., Mainline modules.
+
+package vendor.mediatek.hardware.audio;
+@VintfStability
+interface IAudioParameterChangedCallback {
+  void audioParameterChangedCallback(in String keyValuePairs);
+}

interfaces/hardware/audio/aidl/vendor/mediatek/hardware/audio/IAudioParameterChangedCallback.aidl
@@ -1,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package vendor.mediatek.hardware.audio;
+
+@VintfStability
+interface IAudioParameterChangedCallback {
+    void audioParameterChangedCallback(in String keyValuePairs);
+}